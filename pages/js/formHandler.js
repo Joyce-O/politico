e4066644b8b1fe5091e7e615401437783c15006e
Joyce-O <obijoyce@gmail.com>
2019-01-24 05:24:37 +0100

    [ft-163286057] Add delete modal

---
+++ pages/js/formHandler.js
@@ -0,0 +1,15 @@
+document.getElementById("partyForm").addEventListener("submit", myFunction);
+
+function myFunction(e) {
+    e.preventDefault();
+    window.location.assign("/pages/party_details.html");
+
+}
+
+document.getElementById("Addparty").addEventListener("submit", myFunction);
+
+function myFunction(e) {
+    e.preventDefault();
+    window.location.assign("/pages/party_details.html");
+
+}
