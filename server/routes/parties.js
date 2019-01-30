b4b3eeef56a1130989a794a75ea016e98b24eb59
Joyce-O <obijoyce@gmail.com>
2019-01-31 00:56:27 +0100

    Subject: Implimentation of get all party endpoint  On branch ft-get-a-party-endpoint-163461595  Changes to be committed: 	modified:   server/controllers/PartyController.js 	modified:   server/database/queries.js 	modified:   server/routes/parties.js
    Footer: closes #163461595

--- server/routes/parties.js
+++ server/routes/parties.js
@@ -6,7 +6,7 @@ import { verifyToken } from '../middlewares.js/authorization';
 const parties = express.Router();
 
 parties.post('/', verifyToken, PartyValidation.handleCreateParty, PartyController.createParty);
-parties.get('/', PartyController.getAllParties);
+parties.get('/', verifyToken, PartyController.getAllParties);
 parties.get('/:partyId', PartyController.getOneParty);
 parties.patch('/:partyId/name', PartyValidation.handleEditParty, PartyController.editParty);
 parties.delete('/:partyId', PartyController.deleteParty);
