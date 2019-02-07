6e5f7b13b1354e4edaa0a87237d49a10bc9a38ef
toluola <toluola7@gmail.com>
2019-02-07 12:55:48 +0100

    [ft-163754516] Add api doc

--- server/routes/parties.js
+++ server/routes/parties.js
@@ -1,12 +1,10 @@
 import express from 'express';
 import multer from 'multer';
-import { cloudinaryConfig } from './config/cloudinaryConfig'
 import PartyValidation from '../middlewares.js/PartyValidation';
 import PartyController from '../controllers/PartyController';
 import { verifyToken, adminPass } from '../middlewares.js/authorization';
 
 const parties = express.Router();
-parties.use('/parties', cloudinaryConfig);
 
 const storage = multer.memoryStorage();
 const multerUploads = multer({ storage }).single('logoUrl');
